1e2054f21b1636cb9f08a32cedfe5bfb80153781
Bernardo Marquez <marquez.cmbh@gmail.com>
2023-05-15 15:48:35 -0300

    fix: bugs

--- src/controllers/games.js
+++ src/controllers/games.js
@@ -19,12 +19,13 @@ export async function getGames(req, res){
 export async function insertNewGame(req, res){
     const { name, image, stockTotal, pricePerDay } = req.body;
 
-    try{
-        await db.query(`INSERT INTO games (name, image, "stockTotal", "pricePerDay") 
-                        VALUES ($1, $2, $3, $4, $5)`
-        , [ name, image, stockTotal, pricePerDay ]);
+    try {
+        const games = await db.query(`
+            INSERT INTO games (name, image, "stockTotal", "pricePerDay") 
+            VALUES ($1, $2, $3, $4)
+        `, [name, image, stockTotal, pricePerDay]);
 
-        res.sendStatus(201);
+        res.status(201).json({ games: games.rows });
     }catch(err){
         console.log(err)
         res.sendStatus(500)
